fd20b32867cf57f59756027361724d5b591d5c81
Sohaibkhan100 <sohaibkhan.sk100@gmail.com>
2025-08-24 14:56:03 +0500

    complete security checker

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
 
-// GitHub service import
 let GitHubService;
 try {
     GitHubService = require('./services/github-service');
